7831fdc3dd194468a768eab303c6fb0691d73ea5
Marito <mario_hernan@hotmail.com>
2022-03-24 08:35:30 -0300

    commit para agregar numero con decimal con objeto de DecimalFormat

--- src/unidad4/ProductoImpar.java
+++ src/unidad4/ProductoImpar.java
@@ -1,13 +1,15 @@
 package unidad4;
 
+import java.text.DecimalFormat;
 public class ProductoImpar {
 
     public static void main(String[] args) {
+        DecimalFormat decimal = new DecimalFormat("#,###");
         long producto = 1;
         for (int i = 1; i < 16; i++) {
             producto *= i;
             i++;
         }
-        System.out.printf("El producto entre los enteros impares del 1 al 15 es: %d\n", producto);
+        System.out.printf("El producto entre los enteros impares del 1 al 15 es: %s\n", decimal.format(producto));
     }
 }
