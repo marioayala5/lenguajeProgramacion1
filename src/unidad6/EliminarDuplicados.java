1a206660caae80de5ee34acacb42ff91b131fe71
Mario Ayala <mario_hernan@hotmail.com>
2022-04-20 20:30:30 -0400

    unidad 6 terminado en casa gracias a Charles

--- src/unidad6/EliminarDuplicados.java
+++ src/unidad6/EliminarDuplicados.java
@@ -7,42 +7,44 @@ public class EliminarDuplicados {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int arreglo[] = new int[5];
-        boolean resul;
+        boolean resul = false;
         int nro;
 
         for (int i = 0; i < arreglo.length; i++) {
             System.out.print("Ingrese un numero: ");
             nro = scan.nextInt();
             resul = BuscarDuplicado(nro, arreglo);
-            if (resul == true) {
+            while (resul == true) {
                 System.out.println("Numero Duplicado");
-                i--;
-            } else {
-                arreglo[i] = nro;
+                System.out.printf("Introduzca un numero: ");
+                nro = scan.nextInt();
+                resul = BuscarDuplicado(nro, arreglo);
             }
+            arreglo[i] = nro;
+            resul = false;
         }
-        
+
         System.out.println("");
         System.out.println("Numeros Distintos");
-        
+
         for (int i : arreglo) {
             System.out.printf("%d ", i);
         }
 
     }
 
-    private static boolean BuscarDuplicado(int elemento, int[] arreglo) {
-        boolean valor = true;
-
-        for (int i : arreglo) {
-            System.out.println(i);
-            if (elemento == i) {
-                return valor;
-            } else if (elemento != i) {
-                valor = false;
-                return valor;
-            }
+    public static boolean BuscarDuplicado(int elemento, int[] arreglo) {
+        boolean band=false;
+        int d=0;
+        for (int i=0;i<arreglo.length;i++){
+            d=arreglo[i];
+            if(arreglo[i]!=elemento){
+                band=false;
+            }else{
+                band=true;
+                break;
+            }       
         }
-        return valor;
+        return band;
     }
 }
