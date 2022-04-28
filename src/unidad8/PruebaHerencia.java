0d1aca4b5bc97fa989f2a86e171dc6c542ee8e63
Usuario <Usuario@S4-18>
2022-04-28 18:41:24 -0400

    se terminó la tarea 8 en clase. PD: los metodos void se tiene que imprimir por fuera de lo que hice

--- src/unidad8/PruebaHerencia.java
+++ src/unidad8/PruebaHerencia.java
@@ -4,16 +4,40 @@ package unidad8;
 
     public static void main(String[] args) {
         
-        SeleccionFutbol futbol1 = new Futbolista(1, "Mario", "Ayala", 27, 12, 5);
-        SeleccionFutbol futbol2 = new Entrenador(1, "Mario", "Ayala", 23, "APF");
-        SeleccionFutbol futbol3 = new Masajista(1, "Mario", "Ayala", 27, "Kinesiologo", 5);
+        Futbolista futbol1 = new Futbolista(1, "Derlis", "Gonzalez", 27, 12, 5);
+        Entrenador futbol2 = new Entrenador(1, "Julio", "Caceres", 23, "APF");
+        Masajista futbol3 = new Masajista(1, "Guillermo", "Ortigoza", 27, "Kinesiologo", 5);
         
         System.out.println("Todos los futbolistas comienzan una concentracion.");
-        System.out.printf("%s %s %s",futbol1.getNombre(), futbol1.getApellido(), futbol1.concentrarse());
-//        System.out.printf("%s %s -> %s",futbol1.getNombre(), futbol2.getApellido(), futbol1.concentrarse() );
-//        System.out.printf("%s %s -> %s",futbol1.getNombre(), futbol3.getApellido(), futbol1.concentrarse() );
-                
+        System.out.printf("%s %s -> ",futbol1.getNombre(), futbol1.getApellido());
+        futbol1.concentrarse();
+        System.out.printf("%s %s -> ",futbol2.getNombre(), futbol2.getApellido());
+        futbol2.concentrarse();
+        System.out.printf("%s %s -> ",futbol3.getNombre(), futbol3.getApellido());
+        futbol3.concentrarse();
         
-
+        System.out.println("\nTodos los integrantes viajan para jugar un partido.");
+        System.out.printf("%s %s -> ",futbol1.getNombre(), futbol1.getApellido());
+        futbol1.viajar();
+        System.out.printf("%s %s -> ",futbol2.getNombre(), futbol2.getApellido());
+        futbol2.viajar();
+        System.out.printf("%s %s -> ",futbol3.getNombre(), futbol3.getApellido());
+        futbol3.viajar();
+        
+        System.out.println("\nEntrenamiento: Solamente el entrenador y el futbolista tiene metodos para entrenar.");
+        System.out.printf("%s %s -> ",futbol1.getNombre(), futbol1.getApellido());
+        futbol1.entrenar();
+        System.out.printf("%s %s -> ",futbol2.getNombre(), futbol2.getApellido());
+        futbol2.dirigirEntrenamiento();
+                
+        System.out.println("\nMasaje: Solo el masajista tiene el metodo para dar un masaje.");
+        System.out.printf("%s %s -> ",futbol3.getNombre(), futbol3.getApellido());
+        futbol3.darMasaje();
+        
+        System.out.println("\nPartido de Futbol: Solamente el entrenador y el futbolista tiene metodos para el partido de futbol.");
+        System.out.printf("%s %s -> ",futbol1.getNombre(), futbol1.getApellido());
+        futbol1.jugarPartido();
+        System.out.printf("%s %s -> ",futbol2.getNombre(), futbol2.getApellido());
+        futbol2.dirigirPartido();
     }
 }
