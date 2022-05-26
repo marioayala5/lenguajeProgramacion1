405c3fcd4ca7d015ba34db5664f5116132ea384f
Usuario <Usuario@S4-18>
2022-05-26 18:49:50 -0400

    commit de la clase 26-05-22

---
+++ src/clase11/PruebaBoton.java
@@ -0,0 +1,11 @@
+package clase11;
+
+public class PruebaBoton {
+
+    public static void main(String[] args) {
+        MarcoBoton mb = new MarcoBoton();
+        mb.setVisible(true);
+        mb.setLocationRelativeTo(mb);
+    }
+    
+}
