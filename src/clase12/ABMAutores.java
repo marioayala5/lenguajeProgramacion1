7391d52b20324769ad9403a15952f4835b02d9f1
Mario Ayala <mario_hernan@hotmail.com>
2022-06-20 00:25:24 -0400

    commit en casa 19-06-22 terminado

--- src/clase12/ABMAutores.java
+++ src/clase12/ABMAutores.java
@@ -18,10 +18,10 @@ public class ABMAutores extends javax.swing.JFrame {
     static final String CONTROLADOR = "org.postgresql.Driver";
     static final String URL_DATABASE = "jdbc:postgresql://localhost:5432/libros";
     static final String USUARIO = "postgres";
-    static final String CLAVE = "123456789";
+    static final String CLAVE = "1236987";
 
     //La consulta predeterminada obtiene todos los datos de la tabla autores
-    private final String CONSULTA_PREDETERMINADA = "SELECT * FROM autores";
+    private final String CONSULTA_PREDETERMINADA = "SELECT * FROM autores order by idautor";
     private Autores autores;
     private Connection conexion;
     private Statement instruccion;
@@ -45,7 +45,7 @@ public class ABMAutores extends javax.swing.JFrame {
             Logger.getLogger(ABMAutores.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
@@ -57,11 +57,15 @@ public class ABMAutores extends javax.swing.JFrame {
         bGuardar = new javax.swing.JButton();
         spAutor = new javax.swing.JScrollPane();
         tAutor = new javax.swing.JTable();
+        bModificar = new javax.swing.JButton();
+        bEliminar = new javax.swing.JButton();
         mbMenu = new javax.swing.JMenuBar();
         mArchivo = new javax.swing.JMenu();
         miSalir = new javax.swing.JMenuItem();
         mABM = new javax.swing.JMenu();
         miGuardar = new javax.swing.JMenuItem();
+        miModificar = new javax.swing.JMenuItem();
+        miEliminar = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -71,7 +75,18 @@ public class ABMAutores extends javax.swing.JFrame {
 
         lApellidoPaterno.setText("Apellido Paterno:");
 
+        tfApellidoPaterno.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                tfApellidoPaternoKeyPressed(evt);
+            }
+        });
+
         bGuardar.setText("Guardar");
+        bGuardar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                bGuardarActionPerformed(evt);
+            }
+        });
 
         spAutor.setBorder(javax.swing.BorderFactory.createTitledBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)), "Autores"));
 
@@ -93,6 +108,20 @@ public class ABMAutores extends javax.swing.JFrame {
         });
         spAutor.setViewportView(tAutor);
 
+        bModificar.setText("Modificar");
+        bModificar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                bModificarActionPerformed(evt);
+            }
+        });
+
+        bEliminar.setText("Eliminar");
+        bEliminar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                bEliminarActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout pAutorLayout = new javax.swing.GroupLayout(pAutor);
         pAutor.setLayout(pAutorLayout);
         pAutorLayout.setHorizontalGroup(
@@ -111,7 +140,12 @@ public class ABMAutores extends javax.swing.JFrame {
                                 .addGroup(pAutorLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                                     .addComponent(tfNombrePila)
                                     .addComponent(tfApellidoPaterno, javax.swing.GroupLayout.DEFAULT_SIZE, 200, Short.MAX_VALUE)))
-                            .addComponent(bGuardar))
+                            .addGroup(pAutorLayout.createSequentialGroup()
+                                .addComponent(bGuardar)
+                                .addGap(18, 18, 18)
+                                .addComponent(bModificar)
+                                .addGap(18, 18, 18)
+                                .addComponent(bEliminar)))
                         .addGap(0, 0, Short.MAX_VALUE)))
                 .addContainerGap())
         );
@@ -127,7 +161,10 @@ public class ABMAutores extends javax.swing.JFrame {
                     .addComponent(lApellidoPaterno)
                     .addComponent(tfApellidoPaterno, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
-                .addComponent(bGuardar)
+                .addGroup(pAutorLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(bGuardar)
+                    .addComponent(bModificar)
+                    .addComponent(bEliminar))
                 .addGap(18, 18, 18)
                 .addComponent(spAutor, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
@@ -136,6 +173,11 @@ public class ABMAutores extends javax.swing.JFrame {
         mArchivo.setText("Archivo");
 
         miSalir.setText("Salir");
+        miSalir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                miSalirActionPerformed(evt);
+            }
+        });
         mArchivo.add(miSalir);
 
         mbMenu.add(mArchivo);
@@ -143,7 +185,28 @@ public class ABMAutores extends javax.swing.JFrame {
         mABM.setText("ABM");
 
         miGuardar.setText("Guardar");
+        miGuardar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                miGuardarActionPerformed(evt);
+            }
+        });
         mABM.add(miGuardar);
+
+        miModificar.setText("Modificar");
+        miModificar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                miModificarActionPerformed(evt);
+            }
+        });
+        mABM.add(miModificar);
+
+        miEliminar.setText("Eliminar");
+        miEliminar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                miEliminarActionPerformed(evt);
+            }
+        });
+        mABM.add(miEliminar);
 
         mbMenu.add(mABM);
 
@@ -172,16 +235,64 @@ public class ABMAutores extends javax.swing.JFrame {
     private void tAutorMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tAutorMouseClicked
         int fila = tAutor.getSelectedRow();
         int idAutor = Integer.parseInt(String.valueOf(tAutor.getValueAt(fila, 0)));
+        buscarAutor(idAutor);
     }//GEN-LAST:event_tAutorMouseClicked
 
+    private void bGuardarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bGuardarActionPerformed
+        guardarAutor();
+    }//GEN-LAST:event_bGuardarActionPerformed
+
+    private void tfApellidoPaternoKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_tfApellidoPaternoKeyPressed
+        int keyChar = evt.getKeyChar();
+        if (keyChar == 10) {
+            guardarAutor();
+        }
+    }//GEN-LAST:event_tfApellidoPaternoKeyPressed
+
+    private void miSalirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_miSalirActionPerformed
+        System.exit(0);
+    }//GEN-LAST:event_miSalirActionPerformed
+
+    private void miGuardarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_miGuardarActionPerformed
+        guardarAutor();
+    }//GEN-LAST:event_miGuardarActionPerformed
+
+    private void bModificarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bModificarActionPerformed
+        int fila = tAutor.getSelectedRow();
+        int idAutor = Integer.parseInt(String.valueOf(tAutor.getValueAt(fila, 0)));
+        modificarAutor(idAutor);
+    }//GEN-LAST:event_bModificarActionPerformed
+
+    private void miModificarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_miModificarActionPerformed
+        int fila = tAutor.getSelectedRow();
+        int idAutor = Integer.parseInt(String.valueOf(tAutor.getValueAt(fila, 0)));
+        modificarAutor(idAutor);
+    }//GEN-LAST:event_miModificarActionPerformed
+
+    private void bEliminarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bEliminarActionPerformed
+        int fila = tAutor.getSelectedRow();
+        int idAutor = Integer.parseInt(String.valueOf(tAutor.getValueAt(fila, 0)));
+        eliminarAutor(idAutor);
+    }//GEN-LAST:event_bEliminarActionPerformed
+
+    private void miEliminarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_miEliminarActionPerformed
+        int fila = tAutor.getSelectedRow();
+        int idAutor = Integer.parseInt(String.valueOf(tAutor.getValueAt(fila, 0)));
+        eliminarAutor(idAutor);
+    }//GEN-LAST:event_miEliminarActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton bEliminar;
     private javax.swing.JButton bGuardar;
+    private javax.swing.JButton bModificar;
     private javax.swing.JLabel lApellidoPaterno;
     private javax.swing.JLabel lNombrePila;
     private javax.swing.JMenu mABM;
     private javax.swing.JMenu mArchivo;
     private javax.swing.JMenuBar mbMenu;
+    private javax.swing.JMenuItem miEliminar;
     private javax.swing.JMenuItem miGuardar;
+    private javax.swing.JMenuItem miModificar;
     private javax.swing.JMenuItem miSalir;
     private javax.swing.JPanel pAutor;
     private javax.swing.JScrollPane spAutor;
@@ -205,10 +316,10 @@ public class ABMAutores extends javax.swing.JFrame {
         //crear array de Object con la cantidad de columnas
         Object[] fila = new Object[cantidad];
         //recorre el resultSet
-        while (resultado.next()) {            
+        while (resultado.next()) {
             //obtiene el valor de la columna
             for (int i = 0; i < cantidad; i++) {
-                fila[i] = resultado.getObject(i+1);
+                fila[i] = resultado.getObject(i + 1);
             }
             //agrega la fila al modelo
             modeloTabla.addRow(fila);
@@ -216,12 +327,12 @@ public class ABMAutores extends javax.swing.JFrame {
         //agrega el modelo a la tabla
         tAutor.setModel(modeloTabla);
     }
-    
-    public void guardarAutor () {
+
+    public void guardarAutor() {
         Autores a = new Autores();
         a.setNombrePila(tfNombrePila.getText());
         a.setApellidoPaterno(tfApellidoPaterno.getText());
-        
+
         int respuesta;
         try {
             PreparedStatement ps = conexion.prepareStatement("INSERT INTO autores(nombrepila, apellidopaterno) VALUES (?,?)");
@@ -233,7 +344,7 @@ public class ABMAutores extends javax.swing.JFrame {
             Logger.getLogger(ABMAutores.class.getName()).log(Level.SEVERE, null, ex);
             respuesta = ex.getErrorCode();
         }
-        
+
         if (respuesta == 1) {
             JOptionPane.showMessageDialog(rootPane, "Registro del autor exitoso", "Sistema", JOptionPane.INFORMATION_MESSAGE);
             tfNombrePila.setText(null);
@@ -241,7 +352,57 @@ public class ABMAutores extends javax.swing.JFrame {
             tfNombrePila.grabFocus();
         }
     }
-    
+
+    public void modificarAutor(int idAutor) {
+        Autores a = new Autores();
+        a.setNombrePila(tfNombrePila.getText());
+        a.setApellidoPaterno(tfApellidoPaterno.getText());
+        a.setIdAutor(idAutor);
+
+        int rp;
+        try {
+            PreparedStatement ps = conexion.prepareStatement("UPDATE public.autores SET nombrepila=?, apellidopaterno=? WHERE idautor=?;");
+            ps.setString(1, a.getNombrePila());
+            ps.setString(2, a.getApellidoPaterno());
+            ps.setInt(3, a.getIdAutor());
+            rp = ps.executeUpdate();
+            cargarTabla();
+        } catch (SQLException ex) {
+            Logger.getLogger(ABMAutores.class.getName()).log(Level.SEVERE, null, ex);
+            rp = ex.getErrorCode();
+        }
+
+        if (rp == 1) {
+            JOptionPane.showMessageDialog(rootPane, "Los datos del autor fueron modificados exitosamente", "Sistema", JOptionPane.INFORMATION_MESSAGE);
+            tfNombrePila.setText(null);
+            tfApellidoPaterno.setText(null);
+            tfNombrePila.grabFocus();
+        }
+    }
+
+    public void eliminarAutor(int idAutor) {
+        Autores a = new Autores();
+        a.setIdAutor(idAutor);
+
+        int rp;
+        try {
+            PreparedStatement ps = conexion.prepareStatement("DELETE FROM public.autores WHERE idautor =?");
+            ps.setInt(1, a.getIdAutor());
+            rp = ps.executeUpdate();
+            cargarTabla();
+        } catch (SQLException ex) {
+            Logger.getLogger(ABMAutores.class.getName()).log(Level.SEVERE, null, ex);
+            rp = ex.getErrorCode();
+        }
+
+        if (rp == 1) {
+            JOptionPane.showMessageDialog(rootPane, "El usuario fue borrado de manera exitosa", "Sistema", JOptionPane.INFORMATION_MESSAGE);
+            tfNombrePila.setText(null);
+            tfApellidoPaterno.setText(null);
+            tfNombrePila.grabFocus();
+        }
+    }
+
     private void buscarAutor(int idAutor) {
         try {
             PreparedStatement ps = conexion.prepareStatement("SELECT * FROM autores WHERE idautor= ?");
@@ -259,6 +420,5 @@ public class ABMAutores extends javax.swing.JFrame {
             Logger.getLogger(ABMAutores.class.getName()).log(Level.SEVERE, null, ex);
             autores = null;
         }
-        
     }
 }
